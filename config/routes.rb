dea514da55e91e4d7e1e9df474b08f81e77af45e
Chris Digby <christopherdigby@gmail.com>
2020-11-15 19:38:53 +0000

    Add like and dislike buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :people
   root 'explore#index'
+  post '/like', to: 'explore#like'
+  post '/dislike', to: 'explore#dislike'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
