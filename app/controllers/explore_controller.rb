3b0b699a9ef952261f6616aea4dcce02974a68f2
Chris Digby <christopherdigby@gmail.com>
2020-11-16 16:13:11 +0000

    Work on ajax for like and dislike

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -3,45 +3,50 @@ class ExploreController < ApplicationController
     get_next
   end
 
-  #Get the next person to be shown to the user
-  def get_next
-=begin
-    count = Person.count
-    #Init seen array if undefined
-    session[:seen] ||= []
-    #Reset if everyone has been seen
-    if session[:seen].length >= count
-      session[:seen].clear
-    end
-
-    #Get next unseen person at random
-    found = false
-    id = nil
-    until found
-      id = rand(1..count)
-      unless session[:seen].include?(id)
-        found = true
-      end
-    end
-=end
-    #Init unseen array or repopulate if empty
-    #@unseen ||= []
-    if @unseen == nil
-      @unseen = []
-    end
-    if @unseen.length == 0
-      @unseen = [*1..Person.count]
-    end
-
-    #Get next unseen person at random
-    @person = Person.find_by(id: @unseen.delete_at(rand(@unseen.length)))
-  end
-
   def like
-
+    respond_to do |format|
+      format.js
+    end
   end
 
   def dislike
 
   end
+
+  private
+    #Get the next person to be shown to the user
+    def get_next
+=begin
+      count = Person.count
+      #Init seen array if undefined
+      session[:seen] ||= []
+      #Reset if everyone has been seen
+      if session[:seen].length >= count
+        session[:seen].clear
+      end
+
+      #Get next unseen person at random
+      found = false
+      id = nil
+      until found
+        id = rand(1..count)
+        unless session[:seen].include?(id)
+          found = true
+        end
+      end
+=end
+      #Init unseen array or repopulate if empty
+      #@unseen ||= []
+      if @unseen == nil
+        @unseen = []
+      end
+      if @unseen.length == 0
+        @unseen = [*1..Person.count]
+      end
+
+      #Get next unseen person at random
+      @person = Person.find_by(id: @unseen.delete_at(rand(@unseen.length)))
+    end
+
+
 end
