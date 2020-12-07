553592c8d81fafe7235a617b5be2f813ceab63e3
Chris Digby <christopherdigby@gmail.com>
2020-12-07 21:50:03 +0000

    Account page to edit account details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   #Creating a new user doesn't require logging in otherwise new users wouldn't be able to sign up
   skip_before_action :check_logged_in, only: [:new, :create]
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show]
 
   # GET /users
   # GET /users.json
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
+    @user = User.new(new_params)
 
     respond_to do |format|
       if @user.save
@@ -45,12 +45,12 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
+      if @current_user.update(update_params)
+        format.html { redirect_to '/account', notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @current_user }
       else
         format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.json { render json: @current_user.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -58,9 +58,10 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user.destroy
+    @current_user.destroy
+    session[:user_id] = nil
     respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.html { redirect_to '/', notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -71,8 +72,14 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
-    def user_params
+    # Parameters for new user
+    def new_params
       params.require(:user).permit(:username, :password, :password_confirmation, :name, :dob, :gender)
+    end
+
+    # Parameters for updating user
+    # Cannot change username once user is created
+    def update_params
+      params.require(:user).permit(:password, :password_confirmation, :name, :dob, :gender, :bio)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,21 @@ Rails.application.routes.draw do
 
   #Home routes
   get 'login' => 'home#login'
-  get 'logout' => 'home#logout'
-
   post 'login' => 'home#auth'
+
+  get 'logout' => 'home#logout'
 
   #User routes
   get 'signup' => 'users#new'
-
   post 'signup' => 'users#create'
+
+  get 'account' => 'users#edit'
+  patch 'account' => 'users#update'
+  delete 'account' => 'users#destroy'
 
   #Explore routes
   get 'explore' => 'explore#index'
+
   post 'explore/like' => 'explore#like'
   post 'explore/dislike' => 'explore#dislike'
 
