4bffe1f7fedf37398456539b1e24d4f29983ea01
Chris Digby <christopherdigby@gmail.com>
2020-12-11 23:03:13 +0000

    Add match notifications to explore page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,14 @@
 class UsersController < ApplicationController
   #Creating a new user doesn't require logging in otherwise new users wouldn't be able to sign up
   skip_before_action :check_logged_in, only: [:new, :create]
-  before_action :set_user, only: [:show]
 
-  # GET /users
-  # GET /users.json
-  def index
-    @users = User.all
-  end
-
-  # GET /users/1
-  # GET /users/1.json
-  def show
-  end
-
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
   def edit
   end
 
-  # POST /users
-  # POST /users.json
   def create
     @user = User.new(new_params)
 
@@ -40,12 +24,10 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
       if @current_user.update(update_params)
-        format.html { redirect_to '/account', notice: t('.update_success') }
+        format.html { redirect_to '/profile', notice: t('.update_success') }
       else
         flash.now[:error] = render_to_string :partial => 'partials/errors', :locals => {model: @current_user}
         format.html { render :edit }
@@ -53,8 +35,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     @current_user.destroy
     session[:user_id] = nil
@@ -64,11 +44,6 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
-
     # Parameters for new user
     def new_params
       params.require(:user).permit(:username, :password, :password_confirmation, :name, :dob, :gender)
