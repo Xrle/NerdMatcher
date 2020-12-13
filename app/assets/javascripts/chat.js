67f9b4714329cadb0a85d93a67624676c2965978
Chris Digby <christopherdigby@gmail.com>
2020-12-13 22:10:10 +0000

    Remove unneeded files

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -1,4 +1,5 @@
 $(document).on('turbolinks:load', () => {
+    //Scroll to bottom of chat window on load
     if (window.location.pathname === '/chat/show_messages') {
         $(document).scrollTop($(document).height());
     }
